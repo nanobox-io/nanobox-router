d3630c675d4fae25534c34e0e6d00c80b906b7f4
Lyon Hill <lyondhill@gmail.com>
2014-12-12 09:50:57 -0700

    add in some comments

--- router.go
+++ router.go
@@ -6,7 +6,6 @@ import (
   "io/ioutil"
   "strconv"
   "strings"
-
 )
 
 type Logger interface {
@@ -26,6 +25,7 @@ func (d DevNullLogger) Info(thing string,v ...interface{}) {}
 func (d DevNullLogger) Debug(thing string,v ...interface{}) {}
 func (d DevNullLogger) Trace(thing string,v ...interface{}) {}
 
+// Router is the device by which you create routing rules
 type Router struct {
   log Logger
   Targets map[string]string
@@ -33,6 +33,7 @@ type Router struct {
 
 }
 
+// New creates a new router sets its logger and returns a pointer to the Router object
 
 func New(port int, log Logger) *Router {
   if log == nil {
@@ -45,7 +46,9 @@ func New(port int, log Logger) *Router {
   }
 }
 
-
+// Start fires up the http listener and routes traffic to your targets
+// targets dont have to be set when the start is started 
+// but before the router can route the traffic it needs to know where traffic is going
 func (r *Router) Start() {
   go func () {
     r.log.Info(strconv.Itoa(r.Port))
@@ -54,15 +57,18 @@ func (r *Router) Start() {
   }()
 }
 
+// AddTarget adds a path and target to the router
+// this allows the router to know where traffic is going
 func (r *Router) AddTarget(path, target string) {
   r.Targets[path] = target
 }
 
-func (r *Router) RemoveTarget(path, target string) {
+// RemoveTarget removes a path from the routing table
+func (r *Router) RemoveTarget(path) {
   delete(r.Targets, path)
 }
 
-
+// report is the http handler that does the all the real routing work
 func (r *Router)report(w http.ResponseWriter, req *http.Request){
 
   uri := r.findTarget(req.RequestURI)+req.RequestURI
@@ -97,12 +103,15 @@ func (r *Router)report(w http.ResponseWriter, req *http.Request){
   w.Write(body)
 }
 
+// fatal handle errors and log data
+// It does not stop execution
 func (r *Router) fatal(err error) {
   if err != nil {
-    r.log.Fatal(err.Error())
+    r.log.Error(err.Error())
   }
 }
 
+// copyHeader copies the header from source to dest
 func copyHeader(source http.Header, dest *http.Header){
   for n, v := range source {
       for _, vv := range v {
@@ -111,6 +120,9 @@ func copyHeader(source http.Header, dest *http.Header){
   }
 }
 
+// findTarget starts with the path given and it looks through the paths
+// to find a match. If it cant find it it strips the path one / back 
+// and recursively tries finding something.
 func (r *Router) findTarget(path string) string {
   r.log.Info(path)
   if tar, ok := r.Targets[path]; ok {
