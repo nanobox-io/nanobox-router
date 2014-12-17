69460e66b5c5aa00d6d0563d9ef90829e93ebe9d
sdomino <sdomino@pagodabox.com>
2014-12-17 12:26:04 -0700

    very minor cleanup

--- router.go
+++ router.go
@@ -18,36 +18,42 @@ type Router struct {
 	Port    string
 }
 
-// New creates a new router sets its logger and returns a pointer to the Router object
-func New(port string, log hatchet.Logger) *Router {
-	if log == nil {
-		log = hatchet.DevNullLogger{}
+// New creates a new router sets its logger and returns a pointer to the Router
+// object
+func New(port string, logger hatchet.Logger) *Router {
+
+	if logger == nil {
+		logger = hatchet.DevNullLogger{}
 	}
-  r := &Router{
-    log:     log,
-    Port:    port,
-    Targets: make(map[string]string),
-  }
-  r.start()
+
+	r := &Router{
+		log:     logger,
+		Port:    port,
+		Targets: make(map[string]string),
+	}
+
+	r.start()
+
 	return r
 }
 
-// start fires up the http listener and routes traffic to your targets
-// targets dont have to be set when the start is started
-// but before the router can route the traffic it needs to know where traffic is going
+// start fires up the http listener and routes traffic to your targets targets
+// dont have to be set when the start is started but before the router can route
+// the traffic it needs to know where traffic is going
 func (r *Router) start() {
 	go func() {
-		r.log.Info(r.Port)
-		pHandler := http.HandlerFunc(r.proxy)
-		err := http.ListenAndServe("0.0.0.0:"+r.Port, pHandler)
-    if err != nil {
-      r.log.Error(err.Error())
-    }
+		r.log.Info("Router listening on port: %v", r.Port)
+
+		//
+		err := http.ListenAndServe("0.0.0.0:"+r.Port, http.HandlerFunc(r.proxy))
+		if err != nil {
+			r.log.Error(err.Error())
+		}
 	}()
 }
 
-// AddTarget adds a path and target to the router
-// this allows the router to know where traffic is going
+// AddTarget adds a path and target to the router this allows the router to know
+// where traffic is going
 func (r *Router) AddTarget(path, target string) {
 	r.Targets[path] = target
 }
@@ -58,42 +64,41 @@ func (r *Router) RemoveTarget(path string) {
 }
 
 // proxy is the http handler that does the all the real routing work
-func (r *Router) proxy(w http.ResponseWriter, req *http.Request) {
+func (r *Router) proxy(rw http.ResponseWriter, req *http.Request) {
 
+	//
 	uri := r.findTarget(req.RequestURI) + req.RequestURI
 
 	r.log.Info(req.Method + ": " + uri)
 
-	// if req.Method == "POST" {
-	//   body, err := ioutil.ReadAll(req.Body)
-	//   r.fatal(err)
-	//   r.log.Info("Body: %v\n", string(body));
-	// }
-
-	rr, err := http.NewRequest(req.Method, uri, req.Body)
+	//
+	remote, err := http.NewRequest(req.Method, uri, req.Body)
 	r.fatal(err)
-	copyHeader(req.Header, &rr.Header)
+
+	copyHeader(req.Header, &remote.Header)
 
 	// Create a client and query the target
 	var transport http.Transport
-	resp, err := transport.RoundTrip(rr)
+	res, err := transport.RoundTrip(remote)
 	r.fatal(err)
 
-	r.log.Info("Resp-Headers: %v\n", resp.Header)
+	r.log.Info("Response-Headers: %v\n", res.Header)
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	//
+	body, err := ioutil.ReadAll(res.Body)
 	r.fatal(err)
 
-	dH := w.Header()
-	copyHeader(resp.Header, &dH)
-	dH.Add("Requested-Host", rr.Host)
+	defer res.Body.Close()
 
-	w.Write(body)
+	//
+	local := rw.Header()
+	copyHeader(res.Header, &local)
+	local.Add("Requested-Host", remote.Host)
+
+	rw.Write(body)
 }
 
-// fatal handle errors and log data
-// It does not stop execution
+// fatal handle errors and log data It does not stop execution
 func (r *Router) fatal(err error) {
 	if err != nil {
 		r.log.Error(err.Error())
@@ -109,19 +114,24 @@ func copyHeader(source http.Header, dest *http.Header) {
 	}
 }
 
-// findTarget starts with the path given and it looks through the paths
-// to find a match. If it cant find it it strips the path one / back
-// and recursively tries finding something.
+// findTarget starts with the path given and it looks through the paths to find
+// a match. If it cant find it it strips the path one / back and recursively tries
+// finding something.
 func (r *Router) findTarget(path string) string {
-	r.log.Info(path)
-	if tar, ok := r.Targets[path]; ok {
-		r.log.Info(tar)
-		return tar
+	r.log.Info("Find target for: %v", path)
+
+	//
+	if target, ok := r.Targets[path]; ok {
+		r.log.Info("Found: %v", target)
+
+		return target
 	} else {
 		if path == "/" {
 			return ""
 		}
 		arr := strings.Split(path, "/")
+
+		//
 		return r.findTarget(strings.Join(arr[:len(arr)-2], "/") + "/")
 	}
 }
